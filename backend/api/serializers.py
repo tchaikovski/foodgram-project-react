993b54dc6452f294d44efc387400bccd27e8d9e2
Tchaikovski <v.chaikovsky@orbis3.com>
2023-07-20 00:05:52 +0300

    переделка 28

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -26,8 +26,8 @@ class UserReadSerializer(UserSerializer):
 
     def get_is_subscribed(self, obj):
         request = self.context.get('request')
-        return request and request.user.is_authenticated \
-            and Subscribe.objects.filter(user=request.user, author=obj).exists()
+        return request and request.user.is_authenticated and \
+            Subscribe.objects.filter(user=request.user, author=obj).exists()
         # return False
 
 
@@ -229,7 +229,7 @@ class RecipeReadSerializer(serializers.ModelSerializer):
     #     return user.is_authenticated and ShoppingCart.objects.filter(
     #         user=user, recipe=obj
     #     ).exists()
-    def get_is_favorited(self, obj):
+    def get_is_in_shopping_cart(self, obj):
         request = self.context.get('request')
         return request and request.user.is_authenticated and \
             ShoppingCart.objects.filter(user=request.user, author=obj).exists()
